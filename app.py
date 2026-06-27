44eece5b53ac99aa76530a8adf6d261b3822c0ed
Manoel Carvalho <2003467+manives@users.noreply.github.com>
2026-06-27 01:41:38 -0300

    Change embedding model to HuggingFaceEmbedding

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from llama_index.embeddings.openai import OpenAIEmbedding
 from llama_index.core import Settings
 
-Settings.embed_model = OpenAIEmbedding()
+Settings.embed_model = HuggingFaceEmbedding(model_name="BAAI/bge-small-en-v1.5")
 
 import streamlit as st
 from llama_index.core import VectorStoreIndex, SimpleDirectoryReader
